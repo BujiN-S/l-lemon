757722c1c61b9178806255f017adb891f095d6fa
BujiN-S <rd0252141@gmail.com>
2026-02-26 04:12:10 -0400

    First try in a long time

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Little Lemon Restaurant</h1>
+      <p>Bienvenida al proyecto de Meta Front-End</p>
+    </div>
+  )
+}
+
+export default App
